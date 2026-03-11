85a5004386272148946f0a762adc9461206eaee7
Maxim Frolov <max.fro2021@gmail.com>
2026-03-11 14:27:18 +0100

    feat: added ionic button for quiz creation

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,46 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { Quiz } from 'src/app/models/quiz';
 import { QuizService } from 'src/app/services/quizService';
-import { 
-  IonHeader, 
-  IonToolbar, 
-  IonTitle, 
-  IonContent, 
-  IonGrid, 
-  IonRow,
-  IonCol
-} from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonFab, IonFabButton, IonIcon } from '@ionic/angular/standalone';
 import { QuizCardComponent } from '../quiz-card/quiz-card.component';
-/*
+import { CreateQuizModal } from '../modals/create-quiz.modal';
+import { add } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 
-    
-
-*/
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
   imports: [
-    IonHeader, 
-    IonToolbar, 
-    IonTitle, 
-    IonContent, 
-    IonGrid, 
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonGrid,
     IonRow,
     IonCol,
-    QuizCardComponent
-  ],
+    QuizCardComponent,
+    IonFab,
+    IonFabButton,
+    IonIcon
+],
 })
 export class HomePageComponent  implements OnInit {
   
   quizzes: Quiz[] = [];
   private itemsPerLine = 4;
   private lines: Array<number> = [];
-  constructor(private quizService: QuizService) { }
+  constructor(private quizService: QuizService) { 
+    addIcons({ add });
+  }
   
   ngOnInit() {
     this.quizService.getAll().subscribe((quizzes) => {
       this.quizzes = quizzes;
     });
+  }
+
+  createQuiz() {
+    console.log("Create quiz clicked");
   }
 }
